e199886f996cc74699b37753b853affefffbbdb4
virus <ndongopamsy08@gmail.com>
2026-01-30 00:56:27 +0000

    user_input_exercise

---
+++ exercises/README.md
@@ -0,0 +1,3 @@
+#Description 
+this script input the name and age of a user by asking his name and age 
+and print a hello masage to gree that person 
---
+++ exercises/user_input.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# asking for a user a name 
+echo -e  "hi please enter your name :\c "
+read  name 
+#asking for a user a age 
+echo -e  "hi please enter your age: \c"
+read  age
+
+echo " hello $name ! you are $age years old. welcome to shell scripting "
+
